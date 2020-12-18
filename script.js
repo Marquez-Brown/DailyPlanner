0e778137141579b09e4fcc67094eb86e36ce645e
Juno Brown <amarbrown@gmail.com>
2020-12-17 22:15:37 -0500

    fixed all of the storage issue. project is now fully done

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
+    var containerB = $(".container");
+    var todaysDate = $("#currentDay");
+    todaysDate.text(moment().format("MMMM DD, YYYY HH:mm"))
+    var currentHour = moment().format("HH");
 
-var todaysDate = $("#currentDay");
-todaysDate.text(moment().format("MMMM DD, YYYY HH:mm"))
-var currentHour = moment().format("HH");
-var scheduleData = []
-// console.log(currentHour);
-    retrieve()
+    // console.log(currentHour);
+    //    previousElementSibling
     //work hours
     var hourArray = [09, 10, 11, 12, 13, 14, 15, 16, 17];
     //for loop to create the elements on the page
@@ -23,7 +23,6 @@ var scheduleData = []
 
         //creates the text field
         var textArea = $("<textarea>");
-        textArea.data("scheduleData", scheduleData);
         textArea.addClass("past description col-sm-10");
         row.append(textArea);
         // console.log(textArea)
@@ -35,49 +34,58 @@ var scheduleData = []
         } else if (currentHour === hourArray[i]) {
             textArea.addClass("present");
         }
-
+        //retrieves data
+        if (localStorage.getItem(hourArray[i]) !== null) {
+            var scheduleData = localStorage.getItem(hourArray[i]);
+            textArea.text(scheduleData); 
+        }
         //creates the button
         var button = $("<button>");
         button.addClass("saveBtn col-sm-1 fas fa-save");
-        button.data("hourArr", hourArray[i]);
+        button.data("data-scheduleData", hourArray[i]);
         row.append(button);
+        // retrieve();
         // setting button attribute to text area
         // button.attr("event-data", textArea);
-        
+
         //     console.log(button)
         // })
+    
         $(".container").append(row);
         //when the save button is clicked
-        $(button).on("click", function(event) {
-        event.preventDefault();
-        console.log("you clicked me")
-        scheduleData = textArea.value   
-        console.log(scheduleData)  
-        localStorage.setItem("scheduleData", JSON.stringify(scheduleData));
-
-    });
+        
         // setting the text to add to local storage
         // $("button").on("click", function() {
         //     var textInput = 
         //     textInput = localstorage.setitems("textInput", JSON.stringify(textInput))
-            
+
         // localStorage.setItem("textInput", JSON.stringify(textInput));
-            // textArea.text(textInput);
+        // textArea.text(textInput);
         //     console.log("something")
         //     ;
         // })
-        
+
         //setting get text from local storage
         // function schedule(event) {
-            
+
         // }
         
+        // function retrieve () {
+        //     if (info!== null) {
+        //     var info = JSON.parse(localStorage.getItem(scheduleData));
     }
-    function retrieve () {
-        if (scheduleData!== null) {
-        var scheduledData = JSON.parse(localStorage.getItem(scheduleData));
-    }
-    }
+        $(containerB).on("click", "button", function (event) {
+        event.preventDefault();
+        console.log("you clicked me");
+        scheduleData = $(this.previousElementSibling).val();
+        console.log(scheduleData);
+        var sData = $(this).data("data-scheduleData");
+        localStorage.setItem(sData, scheduleData);
+
+
+    });
 })
+     
+
 
 
